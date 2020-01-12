6191661f01b2841c44c6f2fb81e3bf1a08f0f014
Michael Hammond <mickeymond@gmail.com>
2020-01-12 03:54:42 +0000

    Fixing heroku PORT issue

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const appServer = require('./lib');
+const PORT = process.env.PORT || 3000;
 const { getAllEITs, addEIT, getEIT, updateEIT, deleteEIT } = require('./src/controllers');
 
 const app = appServer();
@@ -13,6 +14,6 @@ app.put('/:id', updateEIT);
 
 app.delete('/:id', deleteEIT);
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 })
